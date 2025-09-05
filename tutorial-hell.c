651be6d7a0e5cf4c459a88fbf575cb2bb000661d
Samuel <cuencosamuelv@gmail.com>
2025-09-05 13:44:43 +0900

    Initial Commit

---
+++ tutorial-hell.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int my_number[14] = 10;
+    char my_string = "Hello, World!";
+    printf("The number is: %d\n", my_number);
+    printf("%s\n", my_string);
+    return 0;
+}
